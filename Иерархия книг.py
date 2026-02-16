2f6dc224ab052fdc6f0105bfaf602cd86a993baf
gurinadashacom-stack <gurina.dasha.com@yandex.ru>
2026-02-16 16:12:08 +0300

    Add files via upload

---
+++ Иерархия книг.py
@@ -0,0 +1,81 @@
+class Book:
+    """ Базовый класс книги. """
+
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        """ Название книги (только чтение) """
+        return self._name
+
+    @property
+    def author(self):
+        """ Автор книги (только чтение) """
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    """ Бумажная книга. """
+
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Количество страниц должно быть положительным целым числом")
+        self._pages = value
+
+    def __str__(self):
+        return f"{super().__str__()}. Страниц: {self.pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    """ Аудиокнига. """
+
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, value):
+        if not isinstance(value, (int, float)) or value <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом")
+        self._duration = float(value)
+
+    def __str__(self):
+        return f"{super().__str__()}. Продолжительность: {self.duration}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
+
+
+if __name__ == "__main__":
+    paper = PaperBook("Война и мир", "Лев Толстой", 1300)
+    audio = AudioBook("Мастер и Маргарита", "Михаил Булгаков", 15.5)
+
+    print(paper)
+    print(repr(paper))
+
+    print(audio)
+    print(repr(audio))
